dd02a727e5c2f35f814d4c4a5fb3a8b672f73658
Antony <javierjordiguevaraolivera@gmail.com>
2026-03-13 21:55:16 -0500

    v3 anto

--- app/iul-es-jd/page.tsx
+++ app/iul-es-jd/page.tsx
@@ -1,4 +1,5 @@
-﻿import IulEsJdClient from "./pageClient";
+import Script from "next/script";
+import IulEsJdClient from "./pageClient";
 
 const GTM_CONTAINERS = ["GTM-KF64LC38", "GTM-NWSJNQMN"] as const;
 
@@ -61,6 +62,18 @@ export default async function IulEsJdPage() {
 
   return (
     <>
+      <Script id="facebook-pixel-pageview" strategy="beforeInteractive">
+        {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '980723860687387');
+fbq('track', 'PageView');`}
+      </Script>
       <LandingGtmScriptsJd />
       <LandingGtmNoscriptJd />
       <noscript>
@@ -82,4 +95,3 @@ export default async function IulEsJdPage() {
     </>
   );
 }
-
--- app/iul-es-pz2/page.tsx
+++ app/iul-es-pz2/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import IulEsPz2Client from "./pageClient";
 
 const GTM_CONTAINERS = ["GTM-KF64LC38", "GTM-NWSJNQMN"] as const;
@@ -61,6 +62,18 @@ export default async function IulEsPz2Page() {
 
   return (
     <>
+      <Script id="facebook-pixel-pageview" strategy="beforeInteractive">
+        {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '980723860687387');
+fbq('track', 'PageView');`}
+      </Script>
       <LandingGtmScriptsPz2 />
       <LandingGtmNoscriptPz2 />
       <noscript>
