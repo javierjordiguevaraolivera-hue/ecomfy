e211e773d530aefa7c0add9c0ff113d9a23af6e2
Antony <javierjordiguevaraolivera@gmail.com>
2026-03-19 10:32:22 -0500

    v3 anto

---
+++ app/efb-pixel-activation/head.tsx
@@ -0,0 +1,15 @@
+const META_PIXEL_INLINE = `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '933274949461490');
+fbq('track', 'PageView');
+fbq('track', 'Lead');`;
+
+export default function Head() {
+  return <script dangerouslySetInnerHTML={{ __html: META_PIXEL_INLINE }} />;
+}
